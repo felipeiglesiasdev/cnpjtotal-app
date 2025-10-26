df28da18a496711ece03689a58bd0a4dc42ba289
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2025-10-26 13:55:40 -0300

    ajuste cache ceps

--- app/Console/Commands/CacheCepsDataCommand.php
+++ app/Console/Commands/CacheCepsDataCommand.php
@@ -22,8 +22,10 @@ class CacheCepsDataCommand extends Command
      * Lista das UFs brasileiras.
      */
     private $ufs = [
-        'AC', 'AL'
-    ];
+            'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO',
+            'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR',
+            'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'
+        ];
 
     /**
      * Executa o comando.
