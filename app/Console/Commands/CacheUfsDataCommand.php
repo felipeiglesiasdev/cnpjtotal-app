c82823d5d1da3b85ddb95ff9ce9e6cbd8938c858
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2025-10-28 16:53:30 -0300

    ajuste cache ufs

--- app/Console/Commands/CacheUfsDataCommand.php
+++ app/Console/Commands/CacheUfsDataCommand.php
@@ -13,7 +13,9 @@ class CacheUfsDataCommand extends Command
     public function handle()
     {
         $ufs = [
-            'AC', 'AL', 'AM'
+            'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO',
+            'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR',
+            'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'
         ];
 
         $this->info('Iniciando cacheamento completo das UFs...');
