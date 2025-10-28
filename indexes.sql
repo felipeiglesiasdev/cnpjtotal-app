8060e61288c74ac4c3dd80f72a8e622086c5cee0
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2025-10-28 15:39:18 -0300

    indexes

--- indexes.sql
+++ indexes.sql
@@ -33,7 +33,6 @@ CREATE INDEX idx_estab_uf_sit_cnae ON estabelecimentos(uf, situacao_cadastral, c
 CREATE INDEX idx_estab_uf_sit_municipio ON estabelecimentos(uf, situacao_cadastral, municipio);
 
 
--- daqui pra baixo, ainda precisa fazer
 CREATE INDEX idx_estab_uf_sit_mun_cep ON estabelecimentos(uf, situacao_cadastral, municipio, cep);
 
 
