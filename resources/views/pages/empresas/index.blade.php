34d41f4c063918b23582f21f8413b94e979782cc
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2025-10-28 14:45:49 -0300

    debug view empresas 1

--- resources/views/pages/empresas/index.blade.php
+++ resources/views/pages/empresas/index.blade.php
@@ -5,21 +5,16 @@
 <div class="bg-white mt-16">
     <div class="container mx-auto px-4 py-12">
         <div class="mt-16">
-            <x-empresas.todos-os-estados />
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-16">
             <div class="lg:col-span-1">
-                <x-empresas.top-10-cidades :cidades="$top10Cidades" /> 
             </div>
             <div class="lg:col-span-1">
-                <x-empresas.top-10-estados :estados="$top10Estados" /> 
             </div>
         </div>
         <div class="mt-16">
-            <x-empresas.estados-mais-fechamentos :estados="$estadosMaisFechamentos" />
         </div>
         <div class="mt-16">
-            <x-empresas.top-10-atividades :atividades="$top10Atividades" />
         </div>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-16">
             <div>
